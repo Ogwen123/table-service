6e80e40563528a9b9c5e040db57fa5a0a00c4346
Owen Jones <owen.d.jones@btinternet.com>
2025-07-06 18:03:36 +0100

    fix new prisma regex

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use("/api/*", async (req, res, next) => {
+app.use("/api/", async (req, res, next) => {
     let enabled
     const enabledRes = (await prisma.services.findUnique({
         where: {
